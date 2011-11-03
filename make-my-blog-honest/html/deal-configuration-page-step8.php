0023fc204ea7875ea3f416ca8557398c58adf917
dan.imbrogno <dan.imbrogno@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-03 21:27:15 +0000

    - initial commit
    git-svn-id: https://plugins.svn.wordpress.org/make-my-blog-honest/tags/1.2@459213 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ make-my-blog-honest/html/deal-configuration-page-step8.php
@@ -0,0 +1,229 @@
+<div class="wrap">
+
+	<h2>
+		
+		<?php _e( 'Deal Configuration Page', self::PREFIX ) ?>
+		
+	</h2>
+	
+	<!-- This is the feedback div -->
+	<div id="feedback">
+	
+		<?php
+			
+			if( array_key_exists( 'error', $this->output) )
+			{
+			
+				echo '<div class="error"><p>'.$this->output['error'].'</p></div>';
+			
+			}
+			
+			if( array_key_exists( 'success', $this->output) )
+			{
+			
+				echo '<div class="updated"><p>'.$this->output['success'].'</p></div>';
+			
+			}
+			
+		?>
+		
+	</div>
+	
+	
+	<h3>
+	
+		<?php _e( 'Add a Deal', 'B2Template' ) ?>
+	
+	</h3>
+	
+	<!-- This is the add a deal form -->
+	<form id="add-deal" class="ajax-form" method="post" action="options-general.php?page=deal-configuration-page">
+	
+		<?php
+
+			$product_name = ( isset( $_POST['product_name'] ) ) ? 
+				$_POST['product_name'] : '';
+
+			$sale_price =  ( isset( $_POST['sale_price'] ) ) ? 
+				$_POST['sale_price'] : '';
+				
+			$enabled =  ( isset( $_POST['enabled'] ) ) ? 
+				true : false;
+				
+			$expires =  ( isset( $_POST['expires'] ) ) ? 
+				$_POST['expires'] : false;
+				
+		?>
+		
+		<p>
+		
+			<label for="product_name"><?php _e( 'Product Name', self::PREFIX ); ?></label>
+			
+			<input type="text" id="product_name" name="product_name" value="<?php echo $product_name; ?>" />
+		
+		</p>
+		
+		<p>
+		
+			<label for="sale_price"><?php _e( 'Sale Price', self::PREFIX ); ?> $</label>
+			
+			<input type="text" id="sale_price" name="sale_price" value="<?php echo $sale_price; ?>" />
+		
+		</p>
+		
+		<p>
+		
+			<label for="enabled"><?php _e( 'Enabled', self::PREFIX ); ?></label>
+			
+			<input type="checkbox" id="enabled" name="enabled" <?php echo ( $enabled !== false ) ? 'checked="checked"' : ''; ?> value="1" />
+		
+		</p>
+
+		
+		<fieldset id="expires">
+		
+			<?php
+			
+				$days = array( 
+					'01','02','03','04','05','06','07','08','09','10',
+					'11','12','13','14','15','16','17','18','19','20',
+					'21','22','23','24','25','26','27','28','29','30','31'
+				);
+				
+				$months = array(
+						'01'=> __( 'January', self::PREFIX),
+						'02'=> __( 'February', self::PREFIX),
+						'03'=> __( 'March', self::PREFIX),
+						'04'=> __( 'April', self::PREFIX),
+						'05'=> __( 'May', self::PREFIX),
+						'06'=> __( 'June', self::PREFIX),
+						'07'=> __( 'July', self::PREFIX),
+						'08'=> __( 'August', self::PREFIX),
+						'09'=> __( 'September', self::PREFIX),
+						'10'=> __( 'October', self::PREFIX),
+						'11'=> __( 'November', self::PREFIX),
+						'12'=> __( 'December', self::PREFIX)
+					);
+					
+					$years = array(
+						'2010', '2011', '2012', '2013', '2014', '2015',
+						'2016', '2017', '2018', '2019', '2020'
+					);
+		
+			?>
+			
+			<legend><?php _e( 'Expires', self::PREFIX ); ?></legend>
+			
+			<select name="expires[Day]">
+			
+				<?php
+				
+					foreach($days as $day)
+					{
+
+						$selected = '';
+
+						if ( 
+							isset($expires['Day']) && 
+							$expires['Day'] == $day 
+						) 
+						{
+						
+							$selected = 'selected="selected"';
+							
+						}
+
+						echo '<option value="'.$day.'" ' . $selected . '>' . $day . '</option>';
+
+					}
+					
+				?>
+				
+			</select>
+			
+			<select name="expires[Month]">
+			
+				<?php
+					
+					foreach($months as $month_num => $month_name)
+					{
+					
+						$selected = '';
+						
+						if( 
+							isset( $expires['Month'] ) && 
+							$expires['Month'] == $month_num 
+						) 
+						{
+						
+							$selected = 'selected="selected"';
+						
+						}
+						
+						echo '<option value="' . $month_num . '" ' . $selected . '>' . $month_name . '</option>';
+						
+					}
+					
+				?>
+				
+			</select>
+			
+			<select name="expires[Year]">
+			
+				<?php
+						
+					foreach($years as $year)
+					{
+					
+						$selected = '';
+						
+						if( 
+							isset( $expires['Year'] ) && 
+							$expires['Year'] == $year 
+						)
+						{
+						
+							 $selected = 'selected="selected"';
+						
+						}
+						
+						echo '<option value="' . $year . '" ' . $selected . '>' . $year . '</option>';
+					}
+					
+				?>
+				
+			</select>
+			
+		</fieldset>
+
+		<p>
+		
+			<input type="hidden" name="<?php echo self::PREFIX . 'Action'; ?>" value="AddDeal" />
+			
+			<input type="hidden" name="_wpnonce" value="<?php echo wp_create_nonce('AddDeal'); ?>" />
+			
+			<input type="submit" class="button-primary" />
+			
+		</p>
+	
+	</form>
+	
+	
+	<h3>
+	
+		<?php _e( 'Deals', 'B2Template' ) ?>
+		
+	</h3>
+	
+	<!-- this is the deal list area -->
+	<div id="deal-list-container">
+	
+		<?php
+		
+			include( 'elements/deal-list.php' );
+		
+		?>
+		
+	</div>
+
+</div>
---
+++ make-my-blog-honest/index.php
@@ -0,0 +1,201 @@
+<?php
+/*
+Plugin Name: Make My Blog Honest
+Plugin URI: http://bloggingsquared.com/plugins/Word11Demo
+Description: This plugin will teach you how to build a plug-in of your own, with Honest Eds as the example client.
+Version: 1.2
+Author: Dan Imbrogno
+Author URI: http://bloggingsquared.com
+License: GPL2
+*/
+
+/* 
+
+	To use this tutorial plugin:
+	
+	1) Activate the plugin
+	2) Open step1.php in your favorite code editor or click "Show Code" in the 
+		bar at the bottom right of your screen
+	3) Look for the NEW CODE and MODIFIED CODE comments. These indicate
+		where code has been updated from the previous step
+	4) When you're ready for the next step click the "next" button in the bar at
+		the bottom left of your screen and repeat
+
+	You can jump to any step at any time with the select box in the bottom 
+	left of your screen
+	
+*/
+
+/* 
+	IGNORE EVERYTHING IN THIS FILE BELOW THIS POINT
+	Let's not get ahead of ourselves! The learning starts inside step1.php,
+*/
+
+if(!class_exists('MakeMyBlogHonestGuide'))
+{
+
+	class MakeMyBlogHonestGuide
+	{
+		
+		const PREFIX = 'mmbhg';
+		
+		var $plugin_url = false;
+		
+		function __construct()
+		{
+			
+			$this->plugin_url = plugins_url(basename(dirname(__FILE__)));
+			
+			add_action('init', array($this,'AddOptions'));			
+			add_action('init', array($this,'RegisterScriptsAndStyles'));
+			add_action('wp_print_styles',array($this,'PrintStyles'));
+			add_action('admin_print_styles',array($this,'PrintStyles'));
+			add_action('wp_print_scripts', array($this,'PrintScripts'));
+			add_action('admin_print_scripts', array($this,'PrintScripts'));
+			
+			$step = $this->GetCurrentStep();
+			
+			$this->LoadPluginStep($step);
+			
+		}
+		
+		function AddOptions()
+		{
+		
+			add_option(self::PREFIX.'_step',1);
+			
+		}
+		
+		function RegisterScriptsAndStyles()
+		{
+
+			wp_register_style(
+				'makemybloghonestguide',
+				$this->plugin_url.'/css/guide.css'
+			);
+			
+			wp_register_script(
+				'makemybloghonestguide',
+				$this->plugin_url.'/js/guide.js',
+				array( 'jquery' ),
+				'1.0',
+				true
+			);
+			
+		}
+		
+		function PrintStyles()
+		{
+		
+			global $pagenow;
+			
+			if('wp-login.php' == $pagenow ) return false; 
+			
+			wp_enqueue_style('makemybloghonestguide');
+			
+		}
+		
+		function PrintScripts()
+		{
+			
+			global $pagenow;
+			
+			if('wp-login.php' == $pagenow ) return false; 
+			
+			$steps = $this->GetStepNames();
+	
+			wp_enqueue_script('makemybloghonestguide');
+			
+			wp_localize_script('makemybloghonestguide', self::PREFIX,
+				 array(
+					'txt_step_select'=>__('Select the plugin step',self::PREFIX),
+					'txt_next'=>__('next',self::PREFIX),
+					'txt_prev'=>__('previous',self::PREFIX),
+					'current_step'=>get_option(self::PREFIX.'_step'),
+					'nonce_change_step'=>wp_create_nonce('ChangeStep'),
+					'l10n_print_after' => self::PREFIX.'_steps = ' . json_encode( $steps ) . ';'
+					
+				)
+			);
+		
+		}
+		
+		private function GetCurrentStep()
+		{
+		
+			if(array_key_exists('mmbhg_step',$_POST))
+			{
+				update_option(self::PREFIX.'_step', $_POST['mmbhg_step']);
+			}
+			
+			$step = get_option(self::PREFIX.'_step');
+			if(false == $step) $step = 1;
+			return $step;
+		}
+		
+		private function LoadPluginStep($step = 1)
+		{
+			if($step == 1)
+			{
+				define( 'WP_UNINSTALL_PLUGIN', true );
+				require_once( 'uninstall.php' );
+			}
+			
+			$this->RequireStep($step);
+			
+		}
+		
+		private function RequireStep($the_step)
+		{
+			require($this->GetStep($the_step));
+		}
+		
+		private function GetStep($the_step)
+		{
+			$steps = $this->GetSteps();
+			if(!array_key_exists($the_step, $steps)) throw new Exception('Step not found');
+			return $steps[$the_step];
+		}
+		
+		public static function GetSteps()
+		{
+			return array(
+				1=>'step1.php',
+				2=>'step2.php',
+				3=>'step3.php',
+				4=>'step4.php',
+				5=>'step5.php',
+				6=>'step6.php',
+				7=>'step7.php',
+				8=>'step8.php',
+				9=>'step9.php',
+				10=>'step10.php',
+				11=>'step11.php'
+			);
+		}
+		
+		public static function GetStepNames()
+		{
+		
+			return array(
+				1=>__('Step 1 - Plugins within classes',self::PREFIX),
+				2=>__('Step 2 - Using filters',self::PREFIX),
+				3=>__('Step 3 - Using actions, attaching JS and CSS',self::PREFIX),
+				4=>__('Step 4 - Configuration pages',self::PREFIX),
+				5=>__('Step 5 - Internationalization',self::PREFIX),
+				6=>__('Step 6 - Creating database tables',self::PREFIX),
+				7=>__('Step 7 - Routing actions',self::PREFIX),
+				8=>__('Step 8 - Database table interaction',self::PREFIX),
+				9=>__('Step 9 - Ajax on admin pages',self::PREFIX),
+				10=>__('Step 10 - Ajax on viewer pages',self::PREFIX),
+				11=>__('Step 11 - Time to experiment!',self::PREFIX)
+			);
+		}
+	
+	}
+	
+	$myBlogHonestGuide = new MakeMyBlogHonestGuide();
+	
+}
+
+?>
