6a6c67b9153f3e80e43e83d0e95b8e7673706b04
dan.imbrogno <dan.imbrogno@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-06 01:04:26 +0000

    - also fixed $wpdb->prepare placeholders
    git-svn-id: https://plugins.svn.wordpress.org/make-my-blog-honest/trunk@459793 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- step8.php
+++ step8.php
@@ -382,7 +382,7 @@ if( !class_exists( 'MakeMyBlogHonest' ) ) {
 			$sql = $wpdb->prepare( 
 				'INSERT INTO ' . $wpdb->prefix . self::PREFIX . 
 					'_deals(`product_name`, `sale_price`, `enabled`, `expires`)'.
-					' VALUES(%s,%s, %s, %s);', 
+					' VALUES(%s, %f, %d, %s);', 
 				$product_name, 
 				$sale_price, 
 				$enabled, 
